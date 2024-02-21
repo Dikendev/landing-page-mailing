1ee235f90e85a1904ea1ac3e666aaf75202e96fc
Dikendev <diken.dev@gmail.com>
2024-02-20 22:48:52 -0300

    feat add validate / create methods

--- src/entities/user/name.ts
+++ src/entities/user/name.ts
@@ -1,3 +1,6 @@
+import { Either, left, right } from '../../shared/either';
+import { InvalidNameError } from './errors/invalid-name';
+
 export class Name {
   private readonly name: string;
 
@@ -6,7 +9,22 @@ export class Name {
     Object.freeze(this);
   }
 
+  static create(name: string): Either<InvalidNameError, Name> {
+    if (!Name.validate(name)) {
+      return left(new InvalidNameError(name));
+    }
+    return right(new Name(name));
+  }
+
   get value(): string {
     return this.name;
+  }
+
+  static validate(name: string): boolean {
+    if (!name || name.trim().length < 2 || name.trim().length > 255) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
