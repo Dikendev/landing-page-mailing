5eb6fa00b4edf76d69d80e6c2256e7e6834b3a64
Dikendev <diken.dev@gmail.com>
2024-02-20 22:50:16 -0300

    feat add user create method

--- src/entities/user/user.ts
+++ src/entities/user/user.ts
@@ -1,4 +1,7 @@
+import { Either, left, right } from '../../shared/either';
 import { Name } from './name';
+import { InvalidNameError } from './errors/invalid-name';
+import { UserData } from './user-data';
 
 export class User {
   public readonly name: Name;
@@ -6,5 +9,18 @@ export class User {
   private constructor(name: Name) {
     this.name = name;
     Object.freeze(this);
+  }
+
+  static create(userData: UserData): Either<InvalidNameError, User> {
+    const nameOrError: Either<InvalidNameError, Name> = Name.create(
+      userData.name,
+    );
+
+    if (nameOrError.isLeft()) {
+      return left(nameOrError.value);
+    }
+
+    const name: Name = nameOrError.value;
+    return right(new User(name));
   }
 }
