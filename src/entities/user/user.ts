955241a1f30738e5b2d2e5f89c6dd72cc1a75f5c
Dikendev <diken.dev@gmail.com>
2024-02-21 23:38:01 -0300

    feat add email param to user class

--- src/entities/user/user.ts
+++ src/entities/user/user.ts
@@ -2,25 +2,40 @@ import { Either, left, right } from '../../shared/either';
 import { Name } from './name';
 import { InvalidNameError } from './errors/invalid-name';
 import { UserData } from './user-data.interface';
+import { Email } from './email';
+import { InvalidEmailError } from './errors/invalid-email';
 
 export class User {
   public readonly name: Name;
+  public readonly email: Email;
 
-  private constructor(name: Name) {
+  private constructor(name: Name, email: Email) {
     this.name = name;
+    this.email = email;
     Object.freeze(this);
   }
 
-  static create(userData: UserData): Either<InvalidNameError, User> {
+  static create(
+    userData: UserData,
+  ): Either<InvalidNameError | InvalidEmailError, User> {
     const nameOrError: Either<InvalidNameError, Name> = Name.create(
       userData.name,
+    );
+
+    const emailOrError: Either<InvalidEmailError, Email> = Email.create(
+      userData.email,
     );
 
     if (nameOrError.isLeft()) {
       return left(nameOrError.value);
     }
 
+    if (emailOrError.isLeft()) {
+      return left(emailOrError.value);
+    }
+
     const name: Name = nameOrError.value;
-    return right(new User(name));
+    const email: Email = emailOrError.value;
+    return right(new User(name, email));
   }
 }
