0017008daf03b964b392b3a76f14bd0ea995c1a5
Dikendev <diken.dev@gmail.com>
2024-02-22 23:53:13 -0300

    feat add not accept more char limit

--- src/entities/user/email-validator.spec.ts
+++ src/entities/user/email-validator.spec.ts
@@ -9,4 +9,10 @@ describe('Email validator', () => {
   test('should not accept email without the at-sign (2)', () => {
     expect(Email.validate('diego.com')).toBe(false);
   });
+
+  test('should not accept more than 64 chars on local part (4)', () => {
+    const localPart = 'c'.repeat(100);
+    const email = localPart + '@gmail.com';
+    expect(Email.validate(email)).toBe(false);
+  });
 });
