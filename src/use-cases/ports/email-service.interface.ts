c10336e3a046089c0697d9dd4351ca599a6b3ffb
Dikendev <diken.dev@gmail.com>
2024-02-22 23:48:43 -0300

    feat add email options interface

---
+++ src/use-cases/ports/email-service.interface.ts
@@ -0,0 +1,21 @@
+import { Either } from '../../shared/either';
+import { MailServiceError } from '../errors/mail-service-error';
+
+export interface EmailOptions {
+  readonly host: string;
+  readonly port: number;
+  readonly username: string;
+  readonly password: string;
+  readonly from: string;
+  readonly to: string;
+  readonly subject: string;
+  readonly text: string;
+  readonly html: string;
+  readonly attachments: object[];
+}
+
+export interface EmailService {
+  send: (
+    options: EmailOptions,
+  ) => Promise<Either<MailServiceError, EmailOptions>>;
+}
