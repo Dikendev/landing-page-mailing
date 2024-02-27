bd8c46f9beaf6469f07133cfd9b4d9b88b13e845
Dikendev <diken.dev@gmail.com>
2024-02-26 23:31:30 -0300

    test add const options

--- src/external/mail-services/nodemailer-email-service.spec.ts
+++ src/external/mail-services/nodemailer-email-service.spec.ts
@@ -1,3 +1,8 @@
+import { MailServiceError } from '../../use-cases/errors/mail-service-error';
+import {
+  ContentType,
+  EmailOptions,
+} from '../../use-cases/ports/email-service.interface';
 import { NodemailerEmailService } from './nodemailer-email-service';
 
 const makeSut = (): NodemailerEmailService => {
@@ -10,6 +15,28 @@ const fromName = 'Test';
 const fromEmail = 'from_email@gmail.com';
 const toName = 'any_name';
 const toEmail = 'any_email@gmail.com';
+const subject = 'test email';
+const emailBody = 'Hello world';
+const emailBodyHtml = '<b> Body Html </b>';
+const attachments = [
+  {
+    filename: attachmentFilePath,
+    contentType: ContentType.txt,
+  },
+];
+
+const mailOptions: EmailOptions = {
+  host: 'host',
+  port: 666,
+  username: 'any_username',
+  password: 'any_password',
+  from: `${fromName} ${fromEmail}`,
+  to: `${toName} ${toEmail}`,
+  subject: subject,
+  text: emailBody,
+  html: emailBodyHtml,
+  attachments: attachments,
+};
 
 jest.mock('nodemailer');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -20,6 +47,10 @@ nodemailer.createTransport.mockReturnValue({ sendMail: sendMailMock });
 beforeEach(() => {
   sendMailMock.mockClear();
   nodemailer.createTransport.mockClear();
+});
+
+afterAll(() => {
+  jest.clearAllMocks();
 });
 
 describe('Nodemailer mail service adapter', () => {
