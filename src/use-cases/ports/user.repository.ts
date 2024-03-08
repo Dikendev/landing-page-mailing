41144c94e90493b4199784c8f961f3734c5c0fc5
Dikendev <diken.dev@gmail.com>
2024-03-07 21:40:16 -0300

    feat add user repository abstract class

--- src/use-cases/ports/user.repository.ts
+++ src/use-cases/ports/user.repository.ts
@@ -1,8 +1,8 @@
 import { UserData } from '../../entities/user/user-data.interface';
 
-export interface UserRepository {
-  findAllUsers: () => Promise<UserData[]>;
-  findUserByEmail: (email: string) => Promise<UserData>;
-  add: (user: UserData) => Promise<void>;
-  exists: (email: string) => Promise<boolean>;
+export abstract class UserRepository {
+  abstract findAllUsers: () => Promise<UserData[]>;
+  abstract findUserByEmail: (email: string) => Promise<UserData>;
+  abstract add: (user: UserData) => Promise<void>;
+  abstract exists: (email: string) => Promise<boolean>;
 }
--- src/usecases/port/user.repository.ts
+++
@@ -1,6 +0,0 @@
-import { User } from 'src/entities/user/user';
-
-export interface UserRepository {
-  findAllUsers: () => User[];
-  add: (user: User) => boolean;
-}
