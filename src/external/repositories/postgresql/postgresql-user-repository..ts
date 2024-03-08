58cf758a9bcd85541abe6296d19cff05e74cecaa
Dikendev <diken.dev@gmail.com>
2024-03-07 21:38:14 -0300

    feat add user repository

--- src/external/repositories/postgresql/postgresql-user-repository..ts
+++ src/external/repositories/postgresql/postgresql-user-repository..ts
@@ -1,9 +1,35 @@
+import { Injectable } from '@nestjs/common';
 import { UserData } from '../../../entities/user/user-data.interface';
 import { UserRepository } from '../../../use-cases/ports/user.repository';
+import { PrismaService } from '../../prisma/prisma.service';
 
+@Injectable()
 export class PostgresqlRepository implements UserRepository {
-  findAllUsers: () => Promise<UserData[]>;
-  findUserByEmail: (email: string) => Promise<UserData>;
-  add: (user: UserData) => Promise<void>;
-  exists: (email: string) => Promise<boolean>;
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAllUsers(): Promise<UserData[]> {
+    return this.prisma.user.findMany();
+  }
+
+  async findUserByEmail(email: string): Promise<UserData> {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    return user;
+  }
+
+  async add(user: UserData): Promise<void> {
+    const exist = this.exists(user.email);
+    if (!exist) {
+      await this.prisma.user.create({ data: user });
+    }
+  }
+
+  async exists(email: string): Promise<boolean> {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user || user?.email === email) {
+      return false;
+    }
+
+    return true;
+  }
 }
